55b4c50886b0dc39e3002ee714da5e3dbf8de7d4
Akhdanzyd <127838277+Akhdanzyd@users.noreply.github.com>
2024-02-13 00:00:37 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -217,7 +217,7 @@
             <div class="float-right d-none d-sm-block">
                 <b>Version</b> 3.2.0
             </div>
-            <strong>Copyright &copy; 2024. Ramadhan Ganteng</strong> All rights
+            <strong>Copyright &copy; 2024. </strong> All rights
             reserved.
         </footer>
 
